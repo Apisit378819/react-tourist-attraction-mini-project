303de9e4ac4054e6676a173c854e455f9e6f9422
Tonkla <Apisit378819@gmail.com>
2024-10-30 20:12:18 +0700

    feat:add search feature for destination listings

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ตำแหน่งที่ตั้งของไฟล์ที่ใช้ Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
